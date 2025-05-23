a9a4a33da80c25b77717d623f0525a5327ab265a
rajyalaxmi29 <rajyalaxmikunchala06@gmail.com>
2025-05-22 20:44:41 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import App1 from "./components/app1";
+import App1 from "./components/App1";
 import App2 from "./components/App2";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
@@ -11,14 +10,14 @@ function App() {
       <header>
         <h1>React Lessons</h1>
         <Link to="/">App1</Link>-
-        <Link to="/app2">App2</Link>
+        <Link to="/App2">App2</Link>
       </header>
       <hr />
       <main>
         <Routes>
           <Route index element={<App1/>}></Route>
           <Route path="/" element={<App1/>}></Route>
-          <Route path="/app2" element={<App2/>}></Route>
+          <Route path="/App2" element={<App2/>}></Route>
         </Routes>
       </main>
       <hr />
