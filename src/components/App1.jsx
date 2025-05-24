0e94ffa93efa6d7750e871ab5e0bb25d8372f4e6
rajyalaxmi29 <rajyalaxmikunchala06@gmail.com>
2025-05-23 19:59:39 -0700

    update

--- src/components/App1.jsx
+++ src/components/App1.jsx
@@ -1,9 +1,14 @@
 import Product from "./Product";
+import Variant from "./Variant";
+
 export default function App1() {
+  
   return (
     <>
-      <div>This is App1 Component</div>
-      <Product />
+      <h1>This is App1 Component</h1>
+      <Product name="Laptop" price={45000} />
+      <Variant />
+      
     </>
   );
 }
