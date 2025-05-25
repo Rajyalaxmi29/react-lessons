3cdb7936465775974f0cf0049b7c4fbcb363b335
rajyalaxmi29 <rajyalaxmikunchala06@gmail.com>
2025-05-25 20:06:40 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import App2 from "./components/App2";
 import App3 from "./components/App3";
 import App4 from "./components/App4";
 import App5 from "./components/App5";
+import App6 from "./components/App6";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
@@ -20,6 +21,7 @@ function App() {
   <Route path="/app3" element={<App3 />} />
     <Route path="/app4" element={<App4 />} />
       <Route path="/app5" element={<App5 />} />
+      <Route path="/app6" element={<App6 />} />
 </Routes>
 
       <Footer />
